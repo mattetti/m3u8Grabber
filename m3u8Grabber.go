f36aaa6db6778345aade5f53a1bcb5a17a6bc268
Matt Aimonetti <mattaimonetti@gmail.com>
2013-07-06 22:11:45 -0700

    started to add a queue mechanism so we will be able to queue up jobs/commands

--- m3u8Grabber.go
+++ m3u8Grabber.go
@@ -10,26 +10,22 @@ import (
 )
 
 // Flags
-var m3u8Url = flag.String("m3u8", "", "Url of the m3u8 file to download.")
-var outputFileName = flag.String("output", "downloaded_video", "The name of the output file without the extension.")
-var httpProxy = flag.String("http_proxy", "", "The url of the HTTP proxy to use")
-var socksProxy = flag.String("socks_proxy", "", "<host>:<port> of the socks5 proxy to use")
-var debug = flag.Bool("debug", false, "Enable debugging messages.")
-var playlist = flag.String("playlist", "", "A list of m3u8 urls to download")
+var (
+  m3u8Url = flag.String("m3u8", "", "Url of the m3u8 file to download.")
+  outputFileName = flag.String("output", "downloaded_video", "The name of the output file without the extension.")
+  httpProxy = flag.String("http_proxy", "", "The url of the HTTP proxy to use")
+  socksProxy = flag.String("socks_proxy", "", "<host>:<port> of the socks5 proxy to use")
+  debug = flag.Bool("debug", false, "Enable debugging messages.")
+  Queue m3u8Utils.Queue
+)
 
 func m3u8ArgCheck() {
-	if *m3u8Url == "" && *playlist == "" {
+	if *m3u8Url == "" {
 		fmt.Fprint(os.Stderr, "You have to pass a m3u8 url file using the right flag.\n")
 		os.Exit(0)
 	}
 }
 
-type PlaylistItem struct {
-	M3u8Url        string
-	OutputFilename string
-	SockProxy      string
-  HttpProxy      string
-}
 
 func downloadM3u8Content(url *string, destFolder string, outputFilename *string, httpProxy *string, socksProxy *string){
   // tmp and final files
--- m3u8Utils/m3u8Utils.go
+++ m3u8Utils/m3u8Utils.go
@@ -3,6 +3,7 @@ package m3u8Utils
 import (
 	"log"
 	"os"
+  "container/list"
 )
 
 func ErrorCheck(err error) {
@@ -15,4 +16,40 @@ func ErrorCheck(err error) {
 func FileAlreadyExists(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
+}
+
+// Queue wrapper on top of a list container
+type Queue struct {
+  list *list.List
+}
+
+func NewCmdQueue() Queue {
+  return Queue{list: list.New()}
+}
+
+func (q Queue) Push(cmd *QueueCommand) {
+  q.list.PushBack(cmd)
+}
+
+func (q Queue) Pop() *QueueCommand {
+  cmdEl := q.list.Front()
+  if cmdEl == nil {
+    return nil
+  }
+  cmd, ok := cmdEl.Value.(*QueueCommand)
+  if ok {
+    q.list.Remove(cmdEl)
+    return cmd
+  } else {
+    return nil
+  }
+}
+
+func (q Queue) Len() int {
+  return q.list.Len()
+}
+
+type QueueCommand struct {
+  M3u8Url string
+  DestinationPath string
 }
