dcb73ce8a8ea57d1ee7306bd07ef0293745407ae
Matt Aimonetti <mattaimonetti@gmail.com>
2014-03-01 17:13:41 -0800

    started working on the http server to receive jobs to queue

--- m3u8Grabber.go
+++ m3u8Grabber.go
@@ -1,36 +1,37 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
-	"github.com/mattetti/m3u8GRabber/m3u8"
-	"github.com/mattetti/m3u8GRabber/m3u8Utils"
 	"log"
 	"os"
-  "errors"
+
+	"github.com/mattetti/m3u8GRabber/m3u8"
+	"github.com/mattetti/m3u8GRabber/m3u8Utils"
 )
 
 // Flags
 var (
-  m3u8Url = flag.String("m3u8", "", "Url of the m3u8 file to download.")
-  outputFileName = flag.String("output", "downloaded_video", "The name of the output file without the extension.")
-  httpProxy = flag.String("http_proxy", "", "The url of the HTTP proxy to use.")
-  socksProxy = flag.String("socks_proxy", "", "<host>:<port> of the socks5 proxy to use.")
-  debug = flag.Bool("debug", false, "Enable debugging messages.")
-  serverPort = flag.Int("server_port", 13535, "The port to run the http server on.")
-  serverMode = flag.Bool("server", false, "Enable running a local web server (not working yet).")
+	m3u8Url        = flag.String("m3u8", "", "Url of the m3u8 file to download.")
+	outputFileName = flag.String("output", "downloaded_video", "The name of the output file without the extension.")
+	httpProxy      = flag.String("http_proxy", "", "The url of the HTTP proxy to use.")
+	socksProxy     = flag.String("socks_proxy", "", "<host>:<port> of the socks5 proxy to use.")
+	debug          = flag.Bool("debug", false, "Enable debugging messages.")
+	serverPort     = flag.Int("server_port", 13535, "The port to run the http server on.")
+	serverMode     = flag.Bool("server", false, "Enable running a local web server (not working yet).")
+	dlRootDir      = flag.String("root_dir", "/tmp", "The root dir to download files to")
 )
 
 func m3u8ArgCheck() {
 	if *m3u8Url == "" && !*serverMode {
 		fmt.Fprint(os.Stderr, "You have to pass a m3u8 url file using the right flag or enable the server mode.\n")
-		os.Exit(0)
+		os.Exit(2)
 	}
 }
 
-
 func downloadM3u8Content(url *string, destFolder string, outputFilename, httpProxy, socksProxy *string) error {
-  // tmp and final files
+	// tmp and final files
 	tmpTsFile := destFolder + "/" + *outputFileName + ".ts"
 	outputFilePath := destFolder + "/" + *outputFileName + ".mkv"
 
@@ -40,31 +41,31 @@ func downloadM3u8Content(url *string, destFolder string, outputFilename, httpPro
 		log.Println("Delete the file if you want to redownload it.\n")
 	} else {
 		segmentUrls, _ := m3u8.SegmentsForUrl(*url, httpProxy, socksProxy)
-    err := m3u8.DownloadSegments(segmentUrls, tmpTsFile, httpProxy, socksProxy)
+		err := m3u8.DownloadSegments(segmentUrls, tmpTsFile, httpProxy, socksProxy)
 		if err != nil {
-      return err
-    }
-    err = m3u8.TsToMkv(tmpTsFile, outputFilePath)
-    if err != nil {
-      return err
-    }
+			return err
+		}
+		err = m3u8.TsToMkv(tmpTsFile, outputFilePath)
+		if err != nil {
+			return err
+		}
 		log.Println("Your file is available here: " + outputFilePath)
 	}
-  return nil
+	return nil
 }
 
 func downloadM3u8ContentWithRetries(url *string, destFolder string, outputFilename, httpProxy, socksProxy *string, retry int) error {
-  var err error
-  if retry < 3 {
-    err = downloadM3u8Content(url, destFolder, outputFilename, httpProxy, socksProxy)
-    if err != nil {
-      log.Printf("ERROR: %s\n", err)
-      err = downloadM3u8ContentWithRetries(url, destFolder, outputFilename, httpProxy, socksProxy, retry + 1)
-    }
-  } else {
-    return errors.New( "Too many retries" )
-  }
-  return err
+	var err error
+	if retry < 3 {
+		err = downloadM3u8Content(url, destFolder, outputFilename, httpProxy, socksProxy)
+		if err != nil {
+			log.Printf("ERROR: %s\n", err)
+			err = downloadM3u8ContentWithRetries(url, destFolder, outputFilename, httpProxy, socksProxy, retry+1)
+		}
+	} else {
+		return errors.New("Too many retries")
+	}
+	return err
 }
 
 func main() {
@@ -82,15 +83,15 @@ func main() {
 	pathToUse, err := os.Getwd()
 	m3u8Utils.ErrorCheck(err)
 
-  if *m3u8Url != "" {
-    err = downloadM3u8ContentWithRetries(m3u8Url, pathToUse, outputFileName, httpProxy, socksProxy, 0)
-    if err != nil {
-      log.Printf("Error downloading %s, error: %s\n", m3u8Url, err)
-    }
-  }
+	if *m3u8Url != "" {
+		err = downloadM3u8ContentWithRetries(m3u8Url, pathToUse, outputFileName, httpProxy, socksProxy, 0)
+		if err != nil {
+			log.Printf("Error downloading %s, error: %s\n", m3u8Url, err)
+		}
+	}
 
-  // server mode
-  if *serverMode {
-    m3u8Utils.StartServer(*serverPort)
-  }
+	// server mode
+	if *serverMode {
+		m3u8Utils.StartServer(*serverPort, *dlRootDir)
+	}
 }
--- m3u8Utils/server.go
+++ m3u8Utils/server.go
@@ -1,27 +1,80 @@
 package m3u8Utils
 
 import (
-  "net/http"
-  "fmt"
-  "log"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-var Jobs Queue
+var queue chan *dlRequest
 
-func StartServer(port int){
-  log.Printf("About to start the server on port %d\n", port)
-  log.Fatal(http.ListenAndServe( fmt.Sprintf(":%d", port), nil))
+type dlRequest struct {
+	Url    string
+	Output string
 }
 
-func init(){
-  Jobs = NewCmdQueue()
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s", "World")
-    // TODO
-    // check post
-    // process params
-    // add to queue
-  })
-  // TODO:
-  // start looping through the queue
+type response struct {
+	Url     string
+	Output  string
+	Message string
+}
+
+func ErrorCheck(err error) {
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+func FileAlreadyExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+func StartServer(port int, rootDir string) {
+	log.Printf("About to start the server on port %d\n", port)
+	queue = make(chan *dlRequest)
+	go receiveJobs(queue)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func receiveJobs(jobChan chan *dlRequest) {
+	fmt.Println("Waiting for a new job ...")
+	for {
+		select {
+		case job := <-jobChan:
+			log.Println(job)
+
+			// TODO: trigger the download job in another go routine
+			//time.Sleep(5000 * time.Millisecond)
+		case <-time.After(30 * time.Second):
+			fmt.Printf(".")
+		}
+	}
+}
+
+// http handler used to add a new job to the queue
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var req dlRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			log.Println(err)
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+		// add to queue
+		queue <- &req
+		res := response{req.Url, req.Output, "Added to the queue"}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+	//
+	fmt.Fprintf(w, "Server running")
+}
+
+func init() {
+	http.HandleFunc("/", mainHandler)
 }
